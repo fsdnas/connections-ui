9c87c4ef8e874f86be0ffa729c9c61ec688708be
nayeemahmedshaik <sheiknayeemahmed@gmail.com>
2022-01-26 23:37:14 +0530

    added post

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
 export class PostComponent implements OnInit {
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+  likeButtonColor: string = 'primary';
 
-  constructor() { }
+  isShown: boolean = false; // hidden by default
+  isLikeClicked: boolean = false;
 
-  ngOnInit(): void {
-  }
+  // like!:any;
 
+  like: string = '';
+  unlike: string = '';
+  shareComponent: string = `success`;
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {}
+  toggleShow = () => {
+    this.isShown = !this.isShown;
+  };
+
+  likeClicked = () => {
+    this.isLikeClicked = !this.isLikeClicked;
+  };
+
+  onShare = () => {
+    this.dialog.open(Popup);
+  };
 }
+@Component({
+  selector: 'popup',
+  templateUrl: 'popup.html',
+})
+export class Popup {}
